1a07bab0a3575c4e1c0385c4fb121f9f4d377d40
Masum Billah <masumbillah6584@gmail.com>
2024-06-27 00:43:32 +0600

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Container from "./components/ui/Container";
 export default function Home() {
   return (<Container>
     <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-    <div>My Future listings</div>
+    <div>My Future Listings Another changed</div>
     </div>
   </Container>
   );
