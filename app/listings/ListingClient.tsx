18ecbb0daeb204a54f729f0d63779a0e0be7e59e
dev-billah <dev.billah@gmail.com>
2024-06-27 21:02:48 +0600

    added reservation booking component

--- app/listings/ListingClient.tsx
+++ app/listings/ListingClient.tsx
@@ -1,19 +1,107 @@
 "use client";
 
-import { Listing, User } from "@prisma/client";
-import React, { useEffect, useMemo, useState } from "react";
+import { Listing, Reservation, User } from "@prisma/client";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { categories } from "../components/shared/navbar/Categories";
 import Container from "../components/ui/Container";
 import ListingHead from "./ListingHead";
 import ListingInfo from "./ListingInfo";
+import useLoginModal from "../hooks/useLoginModal";
+import { useRouter } from "next/navigation";
+import {
+  differenceInCalendarDays,
+  differenceInDays,
+  eachDayOfInterval,
+} from "date-fns";
+import axios from "axios";
+import toast from "react-hot-toast";
+import ListingReservation from "./ListingReservation";
+import { Range } from "react-date-range";
+
 export type ListingWithUser = Listing & { user: User };
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
+};
 
 interface IListing {
   listing: ListingWithUser;
+  reservations?: Reservation[];
   currentUser: User | null;
 }
 
-const ListingClient = ({ listing, currentUser }: IListing) => {
+const ListingClient = ({
+  listing,
+  currentUser,
+  reservations = [],
+}: IListing) => {
+  const loginModal = useLoginModal();
+  const router = useRouter();
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = [];
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      });
+      dates = [...dates, ...range];
+    });
+    return dates;
+  }, [reservations]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+  const [totalPrice, setTotalPrice] = useState(listing.price);
+
+  const onCreateReservation = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+    setIsLoading(true);
+    axios
+      .post("/api/reservations", {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate,
+        listingId: listing.id,
+      })
+      .then(() => {
+        toast.success("Reservation created successfully!");
+        setDateRange(initialDateRange);
+        setTotalPrice(listing.price);
+        router.push(`/listing/${listing.id}`);
+      })
+      .catch(() => {
+        toast.error("Failed to create reservation");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [
+    currentUser,
+    dateRange.startDate,
+    dateRange.endDate,
+    listing.id,
+    listing.price,
+    loginModal,
+    totalPrice,
+    router,
+  ]);
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInCalendarDays(
+        dateRange.startDate,
+        dateRange.endDate
+      );
+      if (dayCount && listing.price) {
+        setTotalPrice(dayCount * listing.price);
+      } else {
+        setTotalPrice(listing.price);
+      }
+    }
+  }, [dateRange, listing.price]);
   const [hasMounted, setHasMounted] = useState(false);
   useEffect(() => {
     setHasMounted(true);
@@ -48,6 +136,17 @@ const ListingClient = ({ listing, currentUser }: IListing) => {
               bathroomCount={listing.bathroomCount}
               locationValue={listing.locationValue}
             />
+            <div className="order-first mb-10 md:order-last md:col-span-3">
+              <ListingReservation
+                price={listing.price}
+                totalPrice={totalPrice}
+                onChangeDate={(value) => setDateRange(value)}
+                dateRange={dateRange}
+                onSubmit={onCreateReservation}
+                disabled={isLoading}
+                disabledDates={disabledDates}
+              />
+            </div>
           </div>
         </div>
       </div>
